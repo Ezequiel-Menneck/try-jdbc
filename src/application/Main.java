f521eee336acef81050dd8cc1ac92dec82d885c5
menneck <zeeeee.peeeeetryyy@gmail.com>
2023-01-09 22:13:05 -0300

    Implements insert function

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,6 +5,7 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.util.Date;
 import java.util.List;
 
 public class Main {
@@ -12,24 +13,27 @@ public class Main {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        System.out.println("=== TEST 1: seller findById ===");
+        System.out.println("=== TEST 1: Seller findById ===");
         Seller seller = sellerDao.findById(2);
         System.out.println(seller);
 
-        System.out.println("\n === TEST 2: seller findByDepartment ===");
+        System.out.println("\n === TEST 2: Seller findByDepartment ===");
         Department department = new Department(2, null);
         List<Seller> sellerList = sellerDao.findByDepartment(department);
         for (Seller obj : sellerList) {
             System.out.println(obj);
         }
 
-        System.out.println("\n === TEST 3: seller findByAll ===");
+        System.out.println("\n === TEST 3: Seller findByAll ===");
         List<Seller> findAll = sellerDao.findAll();
         for (Seller obj2 : findAll) {
             System.out.println(obj2);
         }
 
-
+        System.out.println("\n === TEST 4: Seller Insert ===");
+        Seller newSeller = new Seller(null, "Menneck", "Menneck@menneck.com", new Date(), 10000.00, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
 
     }
 }
